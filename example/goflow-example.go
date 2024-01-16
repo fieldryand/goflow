807d4d5b99f7d32136111326d3214fbe03e0956d
Ryan Field <field.ryan.d@gmail.com>
2024-01-16 20:37:23 +0100

    Use uuid as job id

--- example/goflow-example.go
+++ example/goflow-example.go
@@ -2,24 +2,9 @@ package main
 
 import (
 	"github.com/fieldryand/goflow/v2"
-	"github.com/philippgille/gokv/encoding"
-	"github.com/philippgille/gokv/postgresql"
 )
 
 func main() {
-	storeOptions := postgresql.Options{
-		ConnectionURL:      "postgres://postgres:example@0.0.0.0:5432/postgres?sslmode=disable",
-		TableName:          "Item",
-		MaxOpenConnections: 100,
-		Codec:              encoding.JSON,
-	}
-
-	client, err := postgresql.NewClient(storeOptions)
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-
 	options := goflow.Options{
 		UIPath:       "ui/",
 		Streaming:    true,
@@ -27,7 +12,6 @@ func main() {
 		WithSeconds:  true,
 	}
 	gf := goflow.New(options)
-	gf.AttachStorage(client)
 	gf.Use(goflow.DefaultLogger())
 	gf.Run(":8181")
 }
