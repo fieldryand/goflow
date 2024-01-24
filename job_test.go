a07bdb1a051c19376d06a2e6849def08e97a1624
Ryan Field <field.ryan.d@gmail.com>
2024-01-24 18:11:19 +0100

    Fix

--- job_test.go
+++ job_test.go
@@ -95,7 +95,7 @@ func TestJob(t *testing.T) {
 
 	store := gomap.NewStore(gomap.DefaultOptions)
 
-	go j.run(store, j.newExecution())
+	go j.run(store, j.newExecution(false))
 
 	for {
 		if j.allDone() {
