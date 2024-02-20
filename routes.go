21badf8bef64e451dad09aa1349865b9df12ee7d
Ryan Field <field.ryan.d@gmail.com>
2024-02-20 12:11:21 +0100

    Replace gin with httprouter

--- routes.go
+++ routes.go
@@ -1,210 +1,180 @@
 package goflow
 
 import (
+	"encoding/json"
 	"net/http"
+	"text/template"
 	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/julienschmidt/httprouter"
 )
 
-func (g *Goflow) addStaticRoutes() {
-	g.router.Static("/css", g.Options.UIPath+"css")
-	g.router.Static("/dist", g.Options.UIPath+"dist")
-	g.router.Static("/src", g.Options.UIPath+"src")
-	g.router.LoadHTMLGlob(g.Options.UIPath + "html/*.html.tmpl")
+func (g *Goflow) addTestRoute() {
+	g.Router.GET("/api/jobs", g.handleJobs)
+	g.Router.GET("/api/jobs/:name", g.handleParameterizedJobs)
+	g.Router.POST("/api/jobs/:name/submit", g.handleSubmittedJobs)
+	g.Router.POST("/api/jobs/:name/toggle", g.handleToggledJobs)
+	g.Router.GET("/api/executions", g.handleExecutions)
+	g.Router.GET("/", g.handleRedirect)
+	g.Router.GET("/ui", g.handleRoot)
+	g.Router.GET("/ui/jobs/:name", g.handleJobsPage)
+	g.Router.ServeFiles("/static/*filepath", http.Dir(g.Options.UIPath))
 }
 
-func (g *Goflow) addStreamRoute(keepOpen bool) {
-	g.router.GET("/stream", g.stream(keepOpen))
+//func (g *Goflow) addStreamRoute(keepOpen bool) {
+//	g.router.GET("/stream", g.stream(keepOpen))
+//}
+
+func (g *Goflow) handleRedirect(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.Redirect(w, r, "/ui", http.StatusMovedPermanently)
 }
 
-func (g *Goflow) addUIRoutes() {
-	ui := g.router.Group("/ui")
-	{
-		ui.GET("/", g.handleRoot())
-		ui.GET("/jobs/:name", g.handleJobsPage())
-	}
-
-	g.router.GET("/", handleRedirect)
-}
-
-func (g *Goflow) addAPIRoutes() {
-	api := g.router.Group("/api")
-	{
-		api.GET("/health", handleHealth)
-		api.GET("/jobs", g.handleJobs())
-		api.GET("/jobs/:name", g.handleParameterizedJobs())
-		api.POST("/jobs/:name/submit", g.handleSubmittedJobs())
-		api.POST("/jobs/:name/toggle", g.handleToggledJobs())
-		api.GET("/executions", g.handleExecutions())
-	}
-}
-
-func handleHealth(c *gin.Context) {
+func (g *Goflow) handleJobs(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var msg struct {
-		Health string `json:"health"`
+		Jobs []string `json:"jobs"`
 	}
-	msg.Health = "OK"
-	c.JSON(http.StatusOK, msg)
+	msg.Jobs = g.jobs
+	out, _ := json.Marshal(msg)
+	w.Write(out)
 }
 
-func handleRedirect(c *gin.Context) {
-	c.Redirect(http.StatusFound, "/ui/")
-}
+func (g *Goflow) handleParameterizedJobs(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	name := ps.ByName("name")
+	jobFn, ok := g.Jobs[name]
 
-func (g *Goflow) handleJobs() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var msg struct {
-			Jobs []string `json:"jobs"`
-		}
-		msg.Jobs = g.jobs
-		c.JSON(http.StatusOK, msg)
+	var msg struct {
+		JobName   string   `json:"job"`
+		TaskNames []string `json:"tasks"`
+		Dag       dag      `json:"dag"`
+		Schedule  string   `json:"schedule"`
+		Active    bool     `json:"active"`
 	}
-}
 
-func (g *Goflow) handleParameterizedJobs() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		name := c.Param("name")
-		jobFn, ok := g.Jobs[name]
+	if ok {
+		msg.JobName = name
+		msg.TaskNames = jobFn().tasks
+		msg.Dag = jobFn().Dag
+		msg.Schedule = g.Jobs[name]().Schedule
 
-		var msg struct {
-			JobName   string   `json:"job"`
-			TaskNames []string `json:"tasks"`
-			Dag       dag      `json:"dag"`
-			Schedule  string   `json:"schedule"`
-			Active    bool     `json:"active"`
-		}
-
-		if ok {
-			msg.JobName = name
-			msg.TaskNames = jobFn().tasks
-			msg.Dag = jobFn().Dag
-			msg.Schedule = g.Jobs[name]().Schedule
-
-			// check if the job is active by looking in the list of cron entries
-			for _, entry := range g.cron.Entries() {
-				if jobName := entry.Job.(*scheduledExecution).jobFunc().Name; name == jobName {
-					msg.Active = true
-				}
-			}
-
-			c.JSON(http.StatusOK, msg)
-		} else {
-			c.JSON(http.StatusNotFound, msg)
-		}
-	}
-}
-
-func (g *Goflow) handleSubmittedJobs() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		name := c.Param("name")
-		_, ok := g.Jobs[name]
-
-		var msg struct {
-			Job       string `json:"job"`
-			Success   bool   `json:"success"`
-			Submitted string `json:"submitted"`
-		}
-		msg.Job = name
-
-		if ok {
-			g.execute(name)
-			msg.Success = true
-			msg.Submitted = time.Now().UTC().Format(time.RFC3339Nano)
-			c.JSON(http.StatusOK, msg)
-		} else {
-			msg.Success = false
-			c.JSON(http.StatusNotFound, msg)
-		}
-	}
-}
-
-func (g *Goflow) handleToggledJobs() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		name := c.Param("name")
-		_, ok := g.Jobs[name]
-
-		var msg struct {
-			Job     string `json:"job"`
-			Success bool   `json:"success"`
-			Active  bool   `json:"active"`
-		}
-		msg.Job = name
-
-		if ok {
-			isActive, _ := g.toggle(name)
-			msg.Success = true
-			msg.Active = isActive
-			c.JSON(http.StatusOK, msg)
-		} else {
-			msg.Success = false
-			c.JSON(http.StatusNotFound, msg)
-		}
-	}
-}
-
-func (g *Goflow) handleExecutions() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		jobName := c.Query("jobname")
-		stateQuery := c.Query("state")
-
-		executions := make([]*Execution, 0)
-
-		for job := range g.Jobs {
-			stored, _ := readExecutions(g.Store, job)
-			for _, execution := range stored {
-				if stateQuery != "" && stateQuery != string(execution.State) {
-				} else if jobName != "" && jobName != execution.JobName {
-				} else {
-					executions = append(executions, execution)
-				}
+		// check if the job is active by looking in the list of cron entries
+		for _, entry := range g.cron.Entries() {
+			if jobName := entry.Job.(*scheduledExecution).jobFunc().Name; name == jobName {
+				msg.Active = true
 			}
 		}
 
-		var msg struct {
-			Executions []*Execution `json:"executions"`
-		}
-		msg.Executions = executions
-
-		c.JSON(http.StatusOK, msg)
+		out, _ := json.Marshal(msg)
+		w.Write(out)
+	} else {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	}
 }
 
-func (g *Goflow) handleRoot() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		jobs := make([]*Job, 0)
-		for _, job := range g.jobs {
+func (g *Goflow) handleSubmittedJobs(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	name := ps.ByName("name")
+	_, ok := g.Jobs[name]
 
-			// create the job, assume it's inactive
-			j := g.Jobs[job]()
-			j.Active = false
+	var msg struct {
+		Job       string `json:"job"`
+		Success   bool   `json:"success"`
+		Submitted string `json:"submitted"`
+	}
+	msg.Job = name
 
-			// check if the job is active by looking in the list of cron entries
-			for _, entry := range g.cron.Entries() {
-				if name := entry.Job.(*scheduledExecution).jobFunc().Name; name == j.Name {
-					j.Active = true
-				}
+	if ok {
+		g.execute(name)
+		msg.Success = true
+		msg.Submitted = time.Now().UTC().Format(time.RFC3339Nano)
+		out, _ := json.Marshal(msg)
+		w.Write(out)
+	} else {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+}
+
+func (g *Goflow) handleToggledJobs(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	name := ps.ByName("name")
+	_, ok := g.Jobs[name]
+
+	var msg struct {
+		Job     string `json:"job"`
+		Success bool   `json:"success"`
+		Active  bool   `json:"active"`
+	}
+	msg.Job = name
+
+	if ok {
+		isActive, _ := g.toggle(name)
+		msg.Success = true
+		msg.Active = isActive
+		out, _ := json.Marshal(msg)
+		w.Write(out)
+	} else {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+}
+
+func (g *Goflow) handleExecutions(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	jobName := ps.ByName("jobname")
+	stateQuery := ps.ByName("state")
+
+	executions := make([]*Execution, 0)
+
+	for job := range g.Jobs {
+		stored, _ := readExecutions(g.Store, job)
+		for _, execution := range stored {
+			if stateQuery != "" && stateQuery != string(execution.State) {
+			} else if jobName != "" && jobName != execution.JobName {
+			} else {
+				executions = append(executions, execution)
 			}
-
-			jobs = append(jobs, j)
 		}
-		c.HTML(http.StatusOK, "index.html.tmpl", gin.H{"jobs": jobs})
 	}
+
+	var msg struct {
+		Executions []*Execution `json:"executions"`
+	}
+	msg.Executions = executions
+
+	out, _ := json.Marshal(msg)
+	w.Write(out)
 }
 
-func (g *Goflow) handleJobsPage() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		name := c.Param("name")
-		jobFn, ok := g.Jobs[name]
+func (g *Goflow) handleRoot(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	jobs := make([]*Job, 0)
+	for _, job := range g.jobs {
 
-		if ok {
-			c.HTML(http.StatusOK, "job.html.tmpl", gin.H{
+		// create the job, assume it's inactive
+		j := g.Jobs[job]()
+		j.Active = false
+
+		// check if the job is active by looking in the list of cron entries
+		for _, entry := range g.cron.Entries() {
+			if name := entry.Job.(*scheduledExecution).jobFunc().Name; name == j.Name {
+				j.Active = true
+			}
+		}
+
+		jobs = append(jobs, j)
+	}
+
+	tmpl, _ := template.ParseFiles("ui/html/index.html.tmpl")
+	tmpl.Execute(w, map[string]any{"jobs": jobs})
+}
+
+func (g *Goflow) handleJobsPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	name := ps.ByName("name")
+	jobFn, ok := g.Jobs[name]
+
+	if ok {
+		tmpl, _ := template.ParseFiles("ui/html/job.html.tmpl")
+		tmpl.Execute(w,
+			map[string]any{
 				"jobName":   name,
 				"taskNames": jobFn().tasks,
 				"schedule":  g.Jobs[name]().Schedule,
 			})
-		} else {
-			c.String(http.StatusNotFound, "Not found")
-		}
+	} else {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	}
 }
--- stream.go
+++ stream.go
@@ -1,64 +1,57 @@
 package goflow
 
-import (
-	"io"
-	"time"
-
-	"github.com/gin-gonic/gin"
-)
-
 // Set keepOpen to false when testing--one event will be sent and
 // then the channel is closed by the server.
-func (g *Goflow) stream(keepOpen bool) func(*gin.Context) {
-
-	return func(c *gin.Context) {
-		job := c.Query("jobname")
-
-		history := make([]*Execution, 0)
-
-		// open a channel for live executions
-		chanStream := make(chan *Execution)
-
-		// periodically push the list of job runs into the stream
-		go func() {
-			defer close(chanStream)
-			for {
-				for jobname := range g.Jobs {
-					executions, _ := readExecutions(g.Store, jobname)
-					for _, e := range executions {
-
-						// make sure it wasn't already sent
-						inHistory := false
-
-						for _, h := range history {
-							if e.ID == h.ID && e.ModifiedTimestamp == h.ModifiedTimestamp {
-								inHistory = true
-							}
-						}
-
-						if !inHistory {
-							if job != "" && job == e.JobName {
-								chanStream <- e
-								history = append(history, e)
-							} else if job == "" {
-								chanStream <- e
-								history = append(history, e)
-							}
-						}
-
-					}
-				}
-				time.Sleep(time.Second * 1)
-			}
-		}()
-
-		c.Stream(func(w io.Writer) bool {
-			if msg, ok := <-chanStream; ok {
-				c.SSEvent("message", msg)
-				return keepOpen
-			}
-			return false
-		})
-	}
-
-}
+//func (g *Goflow) stream(keepOpen bool) func(*gin.Context) {
+//
+//	return func(c *gin.Context) {
+//		job := c.Query("jobname")
+//
+//		history := make([]*Execution, 0)
+//
+//		// open a channel for live executions
+//		chanStream := make(chan *Execution)
+//
+//		// periodically push the list of job runs into the stream
+//		go func() {
+//			defer close(chanStream)
+//			for {
+//				for jobname := range g.Jobs {
+//					executions, _ := readExecutions(g.Store, jobname)
+//					for _, e := range executions {
+//
+//						// make sure it wasn't already sent
+//						inHistory := false
+//
+//						for _, h := range history {
+//							if e.ID == h.ID && e.ModifiedTimestamp == h.ModifiedTimestamp {
+//								inHistory = true
+//							}
+//						}
+//
+//						if !inHistory {
+//							if job != "" && job == e.JobName {
+//								chanStream <- e
+//								history = append(history, e)
+//							} else if job == "" {
+//								chanStream <- e
+//								history = append(history, e)
+//							}
+//						}
+//
+//					}
+//				}
+//				time.Sleep(time.Second * 1)
+//			}
+//		}()
+//
+//		c.Stream(func(w io.Writer) bool {
+//			if msg, ok := <-chanStream; ok {
+//				c.SSEvent("message", msg)
+//				return keepOpen
+//			}
+//			return false
+//		})
+//	}
+//
+//}
